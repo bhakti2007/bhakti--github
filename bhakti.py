4e80bf7b8e8176b4bea006222ffed29bc071290a
rishita217 <rishitasakharkar7@gmail.com>
2025-06-20 13:54:45 +0530

    Bhakti first Commit

--- bhakti.py
+++ bhakti.py
@@ -1,13 +1,3 @@
-import os
-from git import Repo
-
-repo = Repo.init(os.getcwd())
-repo.git.checkout("-b", "add-code-files")
-repo.index.add(["file1.py", "file2.py"])
-repo.index.commit("Add initial code files")
-
-origin = repo.create_remote(
-    "origin",
-    f"https://{os.getenv('GITHUB_USER')}:{os.getenv('GITHUB_PAT')}@github.com/{os.getenv('GITHUB_USER')}/{os.getenv('REPO')}.git"
-)
-origin.push(refspec="add-code-files:add-code-files")
+message = "Hello 2025"
+has_digit = any(c.isdigit() for c in message)
+print(has_digit)  # True
